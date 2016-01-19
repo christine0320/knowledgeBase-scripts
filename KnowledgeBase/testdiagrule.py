c46ed4804a373ec07828c2d7439a8655c08b13e8
weishibin <weishibin@ihefe.com>
2016-01-19 15:42:51 +0800

    modify

--- KnowledgeBase/testdiagrule.py
+++ KnowledgeBase/testdiagrule.py
@@ -5,7 +5,7 @@ import pinyin
 
 conn = MongoClient('192.168.10.108',27017)
 db = conn.knowledgeBase
-knowledgeBase = db.diag_rule
+knowledgeBase = db.diag_ruless
 
 level1 = re.compile('^第(.*?)章+(.*)\n')
 level2 = re.compile('^第(.*?)节+(.*)\n')
@@ -23,59 +23,78 @@ value = []
 global words
 global item
 
+def wordslist():
+    wordlist = ()
+    knowdict = open("dictionary/knowledgeBase.txt","r",encoding='utf-8')
+    words = knowdict.readlines()
+    for word in words:
+        if word == "\n":
+            continue
+        word = word.replace("\n","")
+        res = word.split("$$")
+        wordlist += ((res[0],res[1]),)
+    knowdict.close()
+    return(wordlist)
 
 def insertMongo(cont):
+    print(type1+'#########')
+    print(type2+'#########')
     print(type3+'#########')
-    print(cont)
-    # global keyword
-    # global key
-    # global value
-    # keyword = []
-    # tech = []
-    # if cont == None:
-    #     pass
-    # tech = [{"名称":[type3]},]
-    # flaga = 0
-    # for item in content:
-    #     if '【' in item:
-    #         print(content)
-    #         if flaga == 1:
-    #             tech += [{key:value},]
-    #             #print("key=="+key+"value: :"+str(value)+'\n\n\n')
-    #         else: flaga=1
-    #         item = item.replace('【','')
-    #         item = item.replace('】','')
-    #         key = item.replace(' ','')
-    #         key = key.strip('\n')
-    #         value = []
-    #         continue
-    #     else:
-    #         value.append(item)
-    # #print("key=="+key+"value::"+str(value)+'\n\n\n')
-    # tech += [{key:value},]
-    # print(tech)
-    # keyword = set(keyword)
-    # result = []
-    # for i in list(keyword):
-    #     keysss = {}
-    #     keysss["name"] = i[0]
-    #     keysss["datasource"] = i[1]
-    #     result += [keysss,]
-    #
-    # key_word = {"keyword":result}
-    #
-    # tech = {"content":tech}
-    # tech.update(key_word)
-    # rank = (("rankone",type1.strip('\n')),("ranktwo",type2.strip('\n')),("rankthree",type3.strip('\n')),("source","diag_rule"),)
-    # tech.update(dict(rank))
-    # sort = (("sortrankone",pinyin.get(str(type1).strip('\n'))),("sortranktwo",pinyin.get(str(type2).strip('\n'))),("sortrankthree",pinyin.get(str(type3).strip('\n'))),)
-    # index = {"index":type3.strip('\n')}
-    # tech.update(index)
-    # tech.update(sort)
-    # w.write(str(type3).strip('\n')+"$$"+"diag_rule"+"\n")
-    #
-    # #knowledgeBase.insert(tech)
-    # print(tech)
+    global keyword
+    global key
+    global value
+    keyword = []
+    tech = []
+    if cont == None:
+        pass
+    tech = [{"名称":[type3]},]
+    flaga = 0
+    for item in content:
+        words = wordslist()
+        for word in words:
+            name = word[0].strip('\n')
+            name = name.strip(" ")
+            datasource = word[1]
+           # print('--------------%r-------------' % word)
+            if name in item:
+                keyword +=((name,datasource),)
+        if '【' in item:
+            if flaga == 1:
+                tech += [{key:value},]
+                #print("key=="+key+"value: :"+str(value)+'\n\n\n')
+            else: flaga=1
+            item = item.replace('【','')
+            item = item.replace('】','')
+            key = item.replace(' ','')
+            key = key.strip('\n')
+            value = []
+            continue
+        else:
+            value.append(item)
+    #print("key=="+key+"value::"+str(value)+'\n\n\n')
+    tech += [{key:value},]
+    keyword = set(keyword)
+    result = []
+    for i in list(keyword):
+        keysss = {}
+        keysss["name"] = i[0]
+        keysss["datasource"] = i[1]
+        result += [keysss,]
+
+    key_word = {"keyword":result}
+
+    tech = {"content":tech}
+    tech.update(key_word)
+    rank = (("rankone",type1.strip('\n')),("ranktwo",type2.strip('\n')),("rankthree",type3.strip('\n')),("source","diag_rule"),)
+    tech.update(dict(rank))
+    sort = (("sortrankone",pinyin.get(str(type1).strip('\n'))),("sortranktwo",pinyin.get(str(type2).strip('\n'))),("sortrankthree",pinyin.get(str(type3).strip('\n'))),)
+    index = {"index":type3.strip('\n')}
+    tech.update(index)
+    tech.update(sort)
+    #w.write(str(type3).strip('\n')+"$$"+"diag_rule"+"\n")
+
+    knowledgeBase.insert(tech)
+    print(tech)
     # #print(keyword)
 
 while line:
